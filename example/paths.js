bb5a3e91475d83785768229f49a3730d60529423
jkroso <jkroso@gmail.com>
2013-04-30 12:01:00 +1200

    render paths relative to $HOME

--- example/paths.js
+++ example/paths.js
@@ -1,5 +1,7 @@
 var debug = require('../')('paths')
 
 debug('relative to $PWD: %p', __filename)
-debug('outside $PWD: %p', process.env.HOME)
+debug('$HOME: %p', process.env.HOME)
+debug('outside $HOME: %p', process.env.HOME.split('/').slice(0, -1).join('/'))
+debug('inside $HOME: %p', process.env.HOME+'/a/folder')
 debug('encoded url: %u', encodeURIComponent('http://g.com/example'))
