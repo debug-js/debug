79b75ad626efe5f5c05084e1087884ac14fcff45
Nathan Rajlich <nathan@tootallnate.net>
2014-05-30 22:48:44 -0700

    browser: add colors for WebKit browsers
    Based off of the implemention from #67, except this one
    matches the Node.js colored output exactly, including the
    trailing `diff` value.

--- browser.js
+++ browser.js
@@ -11,6 +11,43 @@ exports.save = save;
 exports.load = load;
 
 /**
+ * Colors.
+ */
+
+var colors = [
+  'cyan',
+  'green',
+  'goldenrod', // "yellow" is just too bright on a white background...
+  'blue',
+  'purple',
+  'red'
+];
+
+/**
+ * Previously assigned color.
+ */
+
+var prevColor = 0;
+
+/**
+ * Currently only WebKit-based Web Inspectors are known
+ * to support "%c" CSS customizations.
+ */
+
+var useColors = 'WebkitAppearance' in document.documentElement.style;
+
+/**
+ * Select a color.
+ *
+ * @return {Number}
+ * @api private
+ */
+
+function selectColor() {
+  return colors[prevColor++ % colors.length];
+}
+
+/**
  * Invokes `console.log()` when available.
  * No-op when `console.log` is not a "function".
  *
@@ -18,20 +55,30 @@ exports.load = load;
  */
 
 function log(fmt) {
+  var args = arguments;
   var curr = new Date();
   var ms = curr - (this.prev || curr);
   this.prev = curr;
 
-  fmt = this.namespace
-    + ' '
+  fmt = (useColors ? '%c' : '')
+    + this.namespace
+    + (useColors ? '%c ' : ' ')
     + fmt
-    + ' +' + exports.humanize(ms);
+    + (useColors ? '%c ' : ' ')
+    + '+' + exports.humanize(ms);
+
+  if (useColors) {
+    if (null == this.c) this.c = selectColor();
+    var c = 'color: ' + this.c;
+    args = [args[0], c, ''].concat(Array.prototype.slice.call(arguments, 1));
+    args.push(c);
+  }
 
   // This hackery is required for IE8,
   // where the `console.log` function doesn't have 'apply'
   return 'object' == typeof console
     && 'function' == typeof console.log
-    && Function.prototype.apply.call(console.log, console, arguments);
+    && Function.prototype.apply.call(console.log, console, args);
 }
 
 /**
