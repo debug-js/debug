0a3455a0fd8f9305d6e097dd33f9d78982f96111
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-04-19 18:37:23 -0400

    Debugging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var isRenderer = require('is-electron-renderer');
-if (isRenderer) {
-  module.exports = require('./browser.js');
-} else {
-  module.exports = require('./node.js');
-}
+// var isRenderer = require('is-electron-renderer');
+// if (isRenderer) {
+module.exports = require('./browser.js');
+module.exports.isRenderer = require('is-electron-renderer');
+// } else {
+//   module.exports = require('./node.js');
+// }
