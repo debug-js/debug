436c5a81b4f974c9aef8d5f50e8e69676779b123
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-06-22 13:45:46 -0400

    Finally found time to reorganize code

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// var isRenderer = require('is-electron-renderer');
-// if (isRenderer) {
-module.exports = require('./browser.js');
-module.exports.isRenderer = require('is-electron-renderer');
-// } else {
-//   module.exports = require('./node.js');
-// }
+module.exports = require('./lib');
