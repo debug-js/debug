3cebb51090fb63d341de609b5b30ded26dee291b
Liu Zhanhong <275368990@qq.com>
2016-07-26 13:32:18 +0800

    Fix debug in web worker
    In web worker, document is not accessibility. So check postMessage first.

--- browser.js
+++ browser.js
@@ -39,7 +39,8 @@ exports.colors = [
 
 function useColors() {
   // is webkit? http://stackoverflow.com/a/16459606/376773
-  return ('WebkitAppearance' in document.documentElement.style) ||
+  return ('postMessage' in window) ||
+    ('WebkitAppearance' in document.documentElement.style) ||
     // is firebug? http://stackoverflow.com/a/398120/376773
     (window.console && (console.firebug || (console.exception && console.table))) ||
     // is firefox >= v31?
