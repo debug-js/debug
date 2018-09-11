ef3ec0a9d5bb6c8500056971d3adff66015e0967
Josh Junon <josh@junon.me>
2018-09-10 22:04:31 -0600

    move to XO (closes #397)

--- examples/node/stdout.js
+++ examples/node/stdout.js
@@ -1,16 +1,17 @@
-var debug = require('../../');
-var error = debug('app:error');
+const debug = require('../..');
 
-// by default stderr is used
+const error = debug('app:error');
+
+// By default stderr is used
 error('goes to stderr!');
 
-var log = debug('app:log');
-// set this namespace to log via console.log
-log.log = console.log.bind(console); // don't forget to bind to console!
+const log = debug('app:log');
+// Set this namespace to log via console.log
+log.log = console.log.bind(console); // Don't forget to bind to console!
 log('goes to stdout');
 error('still goes to stderr!');
 
-// set all output to go via console.info
+// Set all output to go via console.info
 // overrides all per-namespace log settings
 debug.log = console.info.bind(console);
 error('now goes to stdout via console.info');
--- examples/node/wildcards.js
+++ examples/node/wildcards.js
@@ -1,10 +1,10 @@
 
-var debug = {
-  foo: require('../../')('test:foo'),
-  bar: require('../../')('test:bar'),
-  baz: require('../../')('test:baz')
+const debug = {
+	foo: require('../..')('test:foo'),
+	bar: require('../..')('test:bar'),
+	baz: require('../..')('test:baz')
 };
 
-debug.foo('foo')
-debug.bar('bar')
-debug.baz('baz')
+debug.foo('foo');
+debug.bar('bar');
+debug.baz('baz');
