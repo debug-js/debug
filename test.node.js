93f6313efd55dbbae8ea4361328b35c2d6446242
Hashim1999164 <sardarhashim30@gmail.com>
2026-07-23 22:26:07 +0500

    fix: honor DEBUG_HIDE_DATE when set as a flag
    Treat an empty DEBUG_HIDE_DATE value as enabled so the documented env var
    works when used without an explicit true/1 value in non-TTY output.

--- test.node.js
+++ test.node.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 
 const assert = require('assert');
+const {spawnSync} = require('child_process');
 const util = require('util');
 const sinon = require('sinon');
 const debug = require('./src/node');
@@ -35,6 +36,34 @@ describe('debug node', () => {
 			log('hello world2');
 			assert.deepStrictEqual(util.formatWithOptions.getCall(0).args[0], options);
 			stdErrWriteStub.restore();
+		});
+
+		it('honors DEBUG_HIDE_DATE when set as a flag', () => {
+			const script = `
+				const tty = require('tty');
+				tty.isatty = () => false;
+				const debug = require('./src/node');
+				debug.enable('test');
+				const log = debug('test');
+				const args = ['hello'];
+				debug.formatArgs.call(log, args);
+				process.stdout.write(args[0]);
+			`;
+
+			for (const hideDate of ['', '1', 'true']) {
+				const result = spawnSync(process.execPath, ['-e', script], {
+					cwd: __dirname,
+					env: {
+						...process.env,
+						DEBUG: 'test',
+						DEBUG_HIDE_DATE: hideDate
+					},
+					encoding: 'utf8'
+				});
+
+				assert.strictEqual(result.status, 0, result.stderr);
+				assert.strictEqual(result.stdout, 'test hello');
+			}
 		});
 	});
 });
